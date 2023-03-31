c3d5cc4d2b739271821939c821ac6b0bc7fd6b39
TRIOLI LOW <trioli.low@gmail.com>
2023-03-31 03:36:26 +0300

    init

---
+++ 3_home_work/3_home_work.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+template<class T>
+class unique {
+private:
+    T* t;
+    unique& operator= (const unique&) = delete;
+public:
+       
+    unique(const T& t_) : t{ new T{t_} } {};
+    ~unique() {
+        if (t) {
+            delete t;
+        }
+    }
+
+    T& operator*() const {
+        return *t;
+    }
+    T* release() {
+        T* r = t;
+        t = nullptr;
+        return r;
+    }
+
+
+};
+
+
+
+int main()
+{
+    std::vector<int> arr;
+    std::vector<int> arr_2;
+
+    unique<std::vector<int>> U(arr);
+    unique<std::vector<int>> U_2(arr_2);
+
+    
+
+    std::cout << "adress arr =" << &(*U);
+    std::cout << std::endl;
+    std::cout << "adress arr_2 =" << &(*U_2);
+    std::cout << std::endl;
+    
+     auto qq = U.release();
+     auto qq_2 = U_2.release();
+
+     std::cout << std::endl;
+     std::cout << "adress arr =" << &(*U);
+     std::cout << std::endl;
+     std::cout << "adress arr_2 =" << &(*U_2);
+     std::cout << std::endl;
+     std::cout << "adress qq =" << qq;
+     std::cout << std::endl;
+     std::cout << "adress qq_2 =" << qq_2;
+
+}
+
